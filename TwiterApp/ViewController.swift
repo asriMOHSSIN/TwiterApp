898d8529e15f2c5f073088917ad489b1c1637c05
mohssin el asri <asrimohssin@gmail.com>
2020-01-27 17:26:34 +0100

    Initial Commit

---
+++ TwiterApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TwiterApp
+//
+//  Created by el asri mohssin on 1/27/20.
+//  Copyright © 2020 el asri mohssin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
